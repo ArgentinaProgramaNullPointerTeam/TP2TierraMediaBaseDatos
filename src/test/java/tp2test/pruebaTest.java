0ebb88c4ac24b719a9447690a5287d527dadbec8
LucianoDomz <72038759+LucianoDomz@users.noreply.github.com>
2021-10-26 14:00:20 -0300

    borrador test

---
+++ src/test/java/tp2test/pruebaTest.java
@@ -0,0 +1,14 @@
+package tp2test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pruebaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
