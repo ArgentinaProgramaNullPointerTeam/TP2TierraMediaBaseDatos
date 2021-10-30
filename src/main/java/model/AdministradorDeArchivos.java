fcfe06aef487674c461c866d290f3fe91950d71c
PatNatMar <87343240+PatNatMar@users.noreply.github.com>
2021-10-30 20:22:10 -0300

    Update AdministradorDeArchivos.java
    Lectura de promociones desde DAO

--- src/main/java/model/AdministradorDeArchivos.java
+++ src/main/java/model/AdministradorDeArchivos.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 import dao.AtraccionDAO;
 import dao.DAOFactory;
+import dao.PromocionDAO;
 import dao.UsuarioDAO;
 
 public class AdministradorDeArchivos {
@@ -19,70 +20,17 @@ public class AdministradorDeArchivos {
 		return usuarioDAO.findAll();
 	}
 
-//LEER ARCHIVO DE ATRACCIONES
 
 	public static List<Atraccion> leerAtracciones() {
 		AtraccionDAO atraccionDAO = DAOFactory.getAtraccionDAO();
 		return atraccionDAO.findAll();
 	}
 
-	@SuppressWarnings("resource")
+	
 	public static List<Promocion> leerPromociones(List<Atraccion> atracciones) {
-		File f = new File("./archivosDeEntrada/promociones.txt");
-		Scanner sc;
-		List<Promocion> promociones = new ArrayList<Promocion>();
-		String[] line;
-
-		try {
-			sc = new Scanner(f);
-
-			while (sc.hasNext()) {
-				line = sc.nextLine().split("-"); // SEPARA LOS ATRIBUTOS - el split cada vez que encuentra un guion,
-													// devuelve un array
-
-				List<Atraccion> atraccionesEnPromo = new ArrayList<Atraccion>();
-				int cantAtracciones = 0;
-				for (int i = 4; i < line.length - 1; i++) {
-					for (Atraccion atraccion : atracciones) {
-						if (atraccion.getNombre().equals(line[i])) {
-							atraccionesEnPromo.add(atraccion);
-						}
-					}if(atraccionesEnPromo.isEmpty()) {
-						throw new Error("La atracción no está en el listado de atracciones");
-					}
-
-				}
-				for (Atraccion atraccion : atracciones) {
-					if (atraccion.getNombre().equals(line[6])) {
-						atraccionesEnPromo.add(atraccion);
-						cantAtracciones = Integer.parseInt(line[3]) + 1;
-					} else {
-						cantAtracciones = Integer.parseInt(line[3]);
-					}
-				}
-				if(line[1].equals("Porcentual")) {
-					Promocion promocionPorcentual = new PromocionPorcentual(line[0], cantAtracciones, atraccionesEnPromo, line[1], line[6]);
-					promociones.add(promocionPorcentual);
-				}else if(line[1].equals("Absoluta")) {
-					Promocion promocionAbsoluta = new PromocionAbsoluta(line[0], cantAtracciones, atraccionesEnPromo, line[1], line[6]);
-					promociones.add(promocionAbsoluta);
-				}else if(line[1].equals("AXB")){
-					Promocion promocionAXB = new PromocionAXB(line[0], cantAtracciones, atraccionesEnPromo, line[1], line[6]);
-					promociones.add(promocionAXB);
-				}
-				
-
-			}
-
-			sc.close();
-
-		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-		} catch (NumberFormatException e) {
-			System.err.println(e.getMessage());
-		}
-
-		return promociones;
+		PromocionDAO promocionDAO = DAOFactory.getPromocionDAO();
+		return promocionDAO.findAll(atracciones);
+		
 	}
 
 	public static void guardarArchivo(ArrayList<String> datosAGuardar, String ruta) {
